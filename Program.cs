12df6a8d0329771266c61a7b0c1781a81e34c1f2
ronaldobergara <bergaraheadbanger@gmail.com>
2024-08-22 11:06:38 -0300

    Carga Inicial

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿var caminhoArquivos = """ """;
+var caminhoDestino  = """ """;
+
+DirectoryInfo dir = new DirectoryInfo(caminhoArquivos);
+
+var listaArquivo = dir.GetFiles("*.xml", SearchOption.AllDirectories);
+
+
+if (listaArquivo.Length > 0)
+{
+
+	foreach (FileInfo arquivo in listaArquivo)
+	{
+		var novoNome = $"{arquivo.Name.Substring(0, 44)}{Path.GetExtension(arquivo.Name)}";
+
+		Console.WriteLine($"{arquivo.FullName} | {novoNome}");
+
+		try
+		{
+			File.Copy(arquivo.FullName, $"{caminhoDestino}\\{novoNome}", overwrite: true);
+		}
+		catch (Exception)
+		{
+			Console.WriteLine($"Erro ao renomear {arquivo.Name} para: {novoNome}");
+		}
+	}
+
+	Console.WriteLine($"{listaArquivo.Length} renomeado(s)");
+	Console.ReadKey();
+}
+else
+{
+	Console.WriteLine("Não foram encontrados arquivos par renomear");
+    Console.ReadKey();
+}
+
+
+    
+
